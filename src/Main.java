e5a30b447f36d4176679e5de1dcf18c122d9fac8
delcarlswlhs <delcarls@wlhs.wlwv.k12.or.us>
2023-04-24 08:51:48 -0700

    added binary (recursive) and linear searches

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,15 @@
-import java.time.Duration;
-import java.time.Instant;
+import java.util.Arrays;
 
 
 public class Main {
     public static void main(String[] args) {
         int[] myNumList = new int[100];
         for(int i = 0; i < myNumList.length; i++){
-            myNumList[i] = (int)(Math.random()*(100)+1);
+            myNumList[i] = (int)(Math.random()*(100)+1); //rand ints between 1 and 100
         }
 
-
-        int randIndex = (int)(Math.random()*(100)+1);
+        Arrays.sort(myNumList);
+        int randIndex = (int)(Math.random()*(myNumList.length));
         int randNumToFind = myNumList[randIndex];
 
         //linear search
@@ -24,11 +23,27 @@ public class Main {
         System.out.println("Elapsed time in ms: " + (end1 - start1));
     }
 
-    public static boolean linearSearch(int[] array, int target){
-        return false;
+    public static int linearSearch(int[] array, int target){
+        for(int i = 0; i<array.length; i++){
+            if(array[i] == target){
+                return i;
+            }
+        }
+        return Integer.MAX_VALUE;
     }
 
-    public static boolean binarySearch(int[] array, int target){
-        return false;
+    //recursive binary search
+    public static int binarySearch(int[] array, int target, int start, int end){
+        int mid = (start+end)/2;
+        if(target==array[mid]){
+            return mid;
+        }
+        if(target<array[mid]){
+            return binarySearch(array,target,start,mid-1);
+        }
+        if(target>array[mid]){
+            return binarySearch(array, target, mid+1, end);
+        }
+        return Integer.MAX_VALUE;
+        }
     }
-}
